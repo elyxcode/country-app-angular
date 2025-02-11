4f424ac63e1f5e1ac993f2719f7ba99a3ec013ca
douglas <dfjuarezreyes21@gmail.com>
2025-02-11 15:13:32 -0600

    by-capital-page component created

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  standalone: false,
+  templateUrl: './by-capital-page.component.html',
+  styles: ``
+})
+export class ByCapitalPageComponent {
+
+}

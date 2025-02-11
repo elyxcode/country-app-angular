d72b271426aae5c75491064c67bcdfdb63d93dde
douglas <dfjuarezreyes21@gmail.com>
2025-02-11 12:26:07 -0600

    siderbar component with links added

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: 'sidebar.component.html',
+  standalone: false,
+})
+export class SidebarComponent { }

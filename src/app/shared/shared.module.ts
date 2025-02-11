2ad7494716c1f391fcf49c45dbf345f6a6fb69c0
douglas <dfjuarezreyes21@gmail.com>
2025-02-11 16:47:42 -0600

    fix remove approuting and set routing module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contactpage/contact-page.component';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
@@ -14,16 +15,18 @@ import { ContactPageComponent } from './pages/contactpage/contact-page.component
     AboutPageComponent,
     ContactPageComponent,
     SidebarComponent,
+    SearchBoxComponent,
   ],
   imports: [
     CommonModule,
-    AppRoutingModule,
+    RouterModule,
   ],
   exports: [
     HomePageComponent,
     AboutPageComponent,
     ContactPageComponent,
     SidebarComponent,
+    SearchBoxComponent,
   ]
 })
 export class SharedModule { }

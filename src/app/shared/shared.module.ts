05621ad389bfd751cccc6dd1962bdbe1eef44470
douglas <dfjuarezreyes21@gmail.com>
2025-02-11 12:27:54 -0600

    import app routing and export about, home and contact component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from '../app-routing.module';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contactpage/contact-page.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
   ],
   exports: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }

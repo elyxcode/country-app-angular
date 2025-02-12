b02c357319b9d94ca39a22568806bc4d3d31a791
douglas <dfjuarezreyes21@gmail.com>
2025-02-12 10:33:51 -0600

    two way binding  input text value

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,7 +7,26 @@ import { Component, Input } from '@angular/core';
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtInput')
+  public txtInput!: ElementRef<HTMLInputElement>;
+
   @Input()
   public placeholder: string = '';
 
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter()
+
+  emitValue(): void {
+
+    const newCountry = this.txtInput.nativeElement.value;
+
+    if(newCountry.length === 0) return;
+
+    this.onValue.emit(newCountry)
+
+    this.txtInput.nativeElement.value = '';
+
+  }
+
 }
+

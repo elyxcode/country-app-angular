e3dac3163022ca78c8c937bb2ba1eb0b68620f13
douglas <dfjuarezreyes21@gmail.com>
2025-02-12 10:34:51 -0600

    http client services import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,7 +16,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

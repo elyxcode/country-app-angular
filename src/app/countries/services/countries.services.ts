3e1f7741c30bf2fc081ffbf80f8ebe36dd16e26c
douglas <dfjuarezreyes21@gmail.com>
2025-02-13 11:41:28 -0600

    search by country and region methods added

--- src/app/countries/services/countries.services.ts
+++ src/app/countries/services/countries.services.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country.interface';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { captureError } from 'rxjs/internal/util/errorContext';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
@@ -13,7 +14,34 @@ export class CountriesService {
   searchCapital(term: string): Observable<Country[]> {
 
     const url: string = `${this._apiUrl}/capital/${term}`;
-    return this.httpClient.get<Country[]>(url);
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        catchError(() => {
+            return of([])
+        })
+      )
+  }
+
+  searchCountry(term: string): Observable<Country[]>{
+    const url: string = `${this._apiUrl}/name/${term}`;
+
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        catchError(() => {
+          return of([])
+        })
+      )
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+    const url: string = `${this._apiUrl}/region/${region}`;
+
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        catchError(() => {
+          return of([])
+        })
+      )
   }
 
 }

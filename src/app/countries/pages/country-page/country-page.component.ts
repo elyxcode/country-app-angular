086c1d1677a8ad1bde650a0c1918b3be8d5e94c7
douglas <dfjuarezreyes21@gmail.com>
2025-02-13 15:46:02 -0600

    OnInit implemented

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.services';
+import { switchMap } from 'rxjs';
 
 @Component({
-  selector: 'app-country-page',
+  selector: 'countries-country-page',
   standalone: false,
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router,
+  ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
+    )
+    .subscribe(
+      (country) => {
+        if(!country){
+          return this.router.navigateByUrl('');
+        }
+
+        return;
+      }
+    )
+  }
 }

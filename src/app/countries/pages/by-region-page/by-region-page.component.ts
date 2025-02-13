8fc4705d31250ccf8d706f377ab82e4f66a27772
douglas <dfjuarezreyes21@gmail.com>
2025-02-13 11:43:00 -0600

    search by region page implemetations

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.services';
 
 @Component({
-  selector: 'app-by-region-page',
+  selector: 'countries-by-region-page',
   standalone: false,
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor(
+    private countriesService: CountriesService,
+  ){}
+
+  searchByRegion(region: string): void {
+
+    this.countriesService.searchRegion(region)
+      .subscribe( countries => {
+        this.countries = countries;
+      })
+  }
 }

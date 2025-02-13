6a0f1b328d4005f8b7a9079ece48d1bfc54458d8
douglas <dfjuarezreyes21@gmail.com>
2025-02-13 11:42:38 -0600

    by country page seach implementations

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.services';
 
 @Component({
-  selector: 'app-by-country-page',
+  selector: 'countries-by-country-page',
   standalone: false,
   templateUrl: './by-country-page.component.html',
   styles: ``
 })
 export class ByCountryPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(
+    private countriesService: CountriesService,
+  ){}
+
+  searchByCountry(term: string){
+    this.countriesService.searchCountry(term)
+     .subscribe(countries => {
+      this.countries = countries;
+     })
+
+  }
 
 }

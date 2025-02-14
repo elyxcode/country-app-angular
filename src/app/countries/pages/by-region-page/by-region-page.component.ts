2d420bba2576d14fe88791d22bf98642e3ee5ae2
douglas <dfjuarezreyes21@gmail.com>
2025-02-14 13:04:40 -0600

    region type remove

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.services';
+import { Region } from '../../interfaces/region.type';
 
-type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
 @Component({
   selector: 'countries-by-region-page',
   standalone: false,

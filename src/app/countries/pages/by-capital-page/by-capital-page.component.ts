9fb925289feff83520b56f2dbf8ee75180868faf
douglas <dfjuarezreyes21@gmail.com>
2025-02-14 13:52:34 -0600

    get countries and term from services throught cache store

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.services';
 import { Country } from '../../interfaces/country.interface';
 
@@ -8,14 +8,20 @@ import { Country } from '../../interfaces/country.interface';
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public initialValue: string = '';
 
   constructor(
     private countriesService: CountriesService,
   ){}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCapital.countries
+    this.initialValue = this.countriesService.cacheStore.byCapital.term;
+  }
 
   searchByCapital(term: string): void {
     this.isLoading = true;

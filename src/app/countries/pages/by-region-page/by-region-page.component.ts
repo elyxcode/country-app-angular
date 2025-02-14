77daa80712692e8edd3df0b74401cb52ef1d72d9
douglas <dfjuarezreyes21@gmail.com>
2025-02-14 14:01:28 -0600

    get countries and region term from countries service

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.services';
 import { Region } from '../../interfaces/region.type';
@@ -9,7 +9,7 @@ import { Region } from '../../interfaces/region.type';
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
@@ -18,6 +18,11 @@ export class ByRegionPageComponent {
   constructor(
     private countriesService: CountriesService,
   ){}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion(region: Region): void {
 

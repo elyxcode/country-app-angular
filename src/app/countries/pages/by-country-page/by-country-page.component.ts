35178728599dc694ac9317f0619a17c875d6a98d
douglas <dfjuarezreyes21@gmail.com>
2025-02-14 13:57:00 -0600

    get country and term data from cache store service

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.services';
 
@@ -8,13 +8,20 @@ import { CountriesService } from '../../services/countries.services';
   templateUrl: './by-country-page.component.html',
   styles: ``
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit{
 
   public countries: Country[] = [];
+
+  public initialValue: string = '';
 
   constructor(
     private countriesService: CountriesService,
   ){}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountries.term;
+  }
 
   searchByCountry(term: string){
     this.countriesService.searchCountry(term)
